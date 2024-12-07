c44b8ef1de386482820bd680773ee5f1c79214da
irfants22 <irfanee02@gmail.com>
2024-12-08 05:07:20 +0700

    update get all polyclinic service

--- src/service/polyclinic-service.js
+++ src/service/polyclinic-service.js
@@ -2,37 +2,33 @@ import { prismaClient } from "../application/db.js";
 import { ResponseError } from "../error/response-error.js";
 
 const getAllPolyclinic = async () => {
-    const polyclinic = await prismaClient.polyclinic.findMany({
-        select: {
-            id: true,
-            name: true,
-            image: true,
-        },
-    });
-
-    return polyclinic;
+  const polyclinics = await prismaClient.polyclinic.findMany({
+    include: {
+      doctors: true,
+    },
+  });
+  return polyclinics;
 };
 
 const deletePolyclinic = async (polyclinicId) => {
-    const polyclinic = await prismaClient.polyclinic.findUnique({
-      where: {
-        id: polyclinicId,
-      },
-    });
-  
-    if (!polyclinic) {
-      throw new ResponseError(404, "Poliklinik tidak ditemukan");
-    }
-  
-    return prismaClient.polyclinic.delete({
-      where: {
-        id: polyclinicId,
-      },
-    });
-  };
-  
+  const polyclinic = await prismaClient.polyclinic.findUnique({
+    where: {
+      id: polyclinicId,
+    },
+  });
+
+  if (!polyclinic) {
+    throw new ResponseError(404, "Poliklinik tidak ditemukan");
+  }
+
+  return prismaClient.polyclinic.delete({
+    where: {
+      id: polyclinicId,
+    },
+  });
+};
 
 export default {
-    getAllPolyclinic,
-    deletePolyclinic,
+  getAllPolyclinic,
+  deletePolyclinic,
 };
