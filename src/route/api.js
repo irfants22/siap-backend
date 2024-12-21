61e1f1fbfeabd25d29d5f7b20f20899b2599db4c
irfants22 <irfanee02@gmail.com>
2024-12-21 17:27:04 +0700

    add isAdmin middleware for some api

--- src/route/api.js
+++ src/route/api.js
@@ -10,27 +10,27 @@ const authorizedRouter = Router();
 authorizedRouter.use(isAuthorized);
 
 // User API
-authorizedRouter.get("/api/users", userController.getAllUser);
+authorizedRouter.get("/api/users", isAdmin, userController.getAllUser);
 authorizedRouter.get("/api/users/me", userController.getUserProfile);
 authorizedRouter.put("/api/users/me", upload.single("image"), userController.updateUserProfile);
 authorizedRouter.delete("/api/users/logout", userController.logoutUser);
-authorizedRouter.delete("/api/users/:userId", userController.deleteUser);
+authorizedRouter.delete("/api/users/:userId", isAdmin, userController.deleteUser);
 
 // Polyclinic API
 authorizedRouter.get("/api/polyclinics", polyclinicController.getAllPolyclinic);
-authorizedRouter.delete("/api/polyclinics/:polyclinicId", polyclinicController.deletePolyclinic);
-authorizedRouter.post("/api/polyclinics", upload.single("image"), polyclinicController.createPolyclinic);
-authorizedRouter.put("/api/polyclinics/:polyclinicId", upload.single("image"), polyclinicController.updatePolyclinic);
+authorizedRouter.post("/api/polyclinics", isAdmin, upload.single("image"), polyclinicController.createPolyclinic);
+authorizedRouter.put("/api/polyclinics/:polyclinicId", isAdmin, upload.single("image"), polyclinicController.updatePolyclinic);
+authorizedRouter.delete("/api/polyclinics/:polyclinicId", isAdmin, polyclinicController.deletePolyclinic);
 
 // Doctor API
 authorizedRouter.get("/api/doctors", doctorController.getAllDoctor);
+authorizedRouter.post("/api/doctors", isAdmin, upload.single("image"), doctorController.createDoctor);
 authorizedRouter.get("/api/doctors/:doctorId", doctorController.getDetailDoctor);
-authorizedRouter.post("/api/doctors", upload.single("image"), doctorController.createDoctor);
-authorizedRouter.put("/api/doctors/:doctorId", upload.single("image"), doctorController.updateDoctor);
-authorizedRouter.delete("/api/doctors/:doctorId", doctorController.deleteDoctor);
+authorizedRouter.put("/api/doctors/:doctorId", isAdmin, upload.single("image"), doctorController.updateDoctor);
+authorizedRouter.delete("/api/doctors/:doctorId", isAdmin, doctorController.deleteDoctor);
 
 // Queue API
-authorizedRouter.get("/api/queues", queueController.getAllQueue);
+authorizedRouter.get("/api/queues", isAdmin, queueController.getAllQueue);
 authorizedRouter.post("/api/queues", queueController.createQueue);
 authorizedRouter.get("/api/queues/me", queueController.getMyQueue);
 authorizedRouter.get("/api/queues/:queueId", queueController.getDetailQueue);
